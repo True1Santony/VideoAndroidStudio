a14a80440a676f28f742f1701970169deb397912
espartaco <espartaconassekine@gmail.com>
2023-12-05 16:06:36 +0100

    Initial commit

---
+++ app/src/main/java/nassekine/spartak/video/MainActivity.java
@@ -0,0 +1,53 @@
+package nassekine.spartak.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnReproducir;
+    VideoView vwReproductor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnReproducir=findViewById(R.id.btnReproducir);
+        vwReproductor=findViewById(R.id.vwReproductor);
+
+        btnReproducir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int aleatorio=(int)Math.floor(Math.random()*2);
+                int identidicadorRecurso;
+
+                if(aleatorio==0){
+
+                    identidicadorRecurso=R.raw.dados;
+
+                }else{
+
+                    identidicadorRecurso=R.raw.bacas;
+
+                }
+
+                String rutaVideo="android.resource://"+getPackageName()+"/"+identidicadorRecurso;
+
+                Uri uri= Uri.parse(rutaVideo);
+
+                vwReproductor.setVideoURI(uri);
+                vwReproductor.start();
+            }
+        });
+
+
+    }
+}
